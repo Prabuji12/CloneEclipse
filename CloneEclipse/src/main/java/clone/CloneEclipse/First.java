9d59858836c614be06a19d36adde41eb7b71aed1
Prabuji12 <sp12prabu@gmail.com>
2020-02-11 11:29:12 +0530

    First commit

---
+++ CloneEclipse/src/main/java/clone/CloneEclipse/First.java
@@ -0,0 +1,14 @@
+package clone.CloneEclipse;
+
+import org.testng.annotations.Test;
+
+public class First {
+
+	@Test
+	public void test() {
+		System.out.println("Test1");
+		// TODO Auto-generated method stub
+
+	}
+
+}
